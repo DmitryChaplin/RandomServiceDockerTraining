a9b97b2caf44b5294e6c38a20bade0a0352cd3c1
Dmitry <dmitry@instigatemobile.com>
2022-04-05 11:46:15 +0400

    Add project files.

---
+++ RandomService.Api/RandomProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace RandomService.Api
+{
+    public class RandomProvider
+    {
+        private static readonly Random instance = new();
+
+        public int Get(int min, int max) => instance.Next(min, max);
+    }
+}
